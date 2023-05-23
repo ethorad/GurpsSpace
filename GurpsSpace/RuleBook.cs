650a2662982c71aa5182683350aa686b033a3a2b
ethorad <donald.fgt@gmail.com>
2023-05-23 06:44:33 +0100

    added user spaceport selection (and some roman numeral helper code)

--- GurpsSpace/RuleBook.cs
+++ GurpsSpace/RuleBook.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Numerics;
+using System.Windows.Controls;
 
 namespace GurpsSpace
 {
@@ -430,6 +431,41 @@ namespace GurpsSpace
             value = (ulong)(Math.Round((double)value, sigFig));
             value *= magnitude;
             return value;
+        }
+        public static string ToRoman(int value)
+        {
+            if (value < 0 || value > 3999)
+                return "ERR";
+            if (value == 0)
+                return "";
+            if (value >= 1000)
+                return "M" + ToRoman(value - 1000);
+            if (value >= 900)
+                return "CM" + ToRoman(value - 900);
+            if (value >= 500)
+                return "D" + ToRoman(value - 500);
+            if (value >= 400)
+                return "CD" + ToRoman(value - 400);
+            if (value >= 100)
+                return "C" + ToRoman(value - 100);
+            if (value >= 90)
+                return "XC" + ToRoman(value - 90);
+            if (value >= 50)
+                return "L" + ToRoman(value - 50);
+            if (value >= 40)
+                return "XL" + ToRoman(value - 40);
+            if (value >= 10)
+                return "X" + ToRoman(value - 10);
+            if (value >= 9)
+                return "IX" + ToRoman(value - 9);
+            if (value >= 5)
+                return "V" + ToRoman(value - 5);
+            if (value >= 4)
+                return "IV" + ToRoman(value - 4);
+            if (value >= 1)
+                return "I" + ToRoman(value - 1);
+            else
+                return "ERR";
         }
 
         static RuleBook()
